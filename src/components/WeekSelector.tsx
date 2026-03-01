29d76f1c9326919ac5615092a03701c06cdb6ba6
roccostager <99860439+roccostager@users.noreply.github.com>
2026-03-01 20:49:38 +1100

    Normalise WeekSelector week numbers

--- src/components/WeekSelector.tsx
+++ src/components/WeekSelector.tsx
@@ -9,9 +9,6 @@ const WEEKS = [1, 2, 3, 4, 5, 7, 8, 9, 10, 0];
 function WeekSelector({ week, setWeek }: propTypes) {
     return (<select name="" id="" className="focus:outline-0" value={week} onChange={event => setWeek(parseInt(event.target.value))}>
         {WEEKS.map((value, index) => {
-            if (value === 0) {
-                return <option key={index} value={value}>N/A</option>;
-            }
             return <option key={index} value={value}>Week {value}</option>
         })}
     </select>)
