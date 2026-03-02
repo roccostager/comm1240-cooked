e1368b542846bd47287a59397777dd051e9002c4
roccostager <99860439+roccostager@users.noreply.github.com>
2026-03-02 17:39:01 +1100

    Update credit

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -7,6 +7,7 @@ interface propTypes {
 function Content({ week }: propTypes) {
     return (<div className="max-w-5xl m-auto min-h-full pt-4 px-4 md:px-10">
         <p className="font-serif text-3xl">Week {week} Questions</p>
+        <p className="font-serif">Source: <a className="text-sky-600 hover:underline" href="https://actuaries.org.uk/past-exam-papers-and-examiners-reports/" rel="noreferrer" target="_blank">Institute and Faculty of Actuaries</a>.</p>
 
         <DisplayQuestions week={week} />
     </div>)
